6eefc028498290b2f71cdbd5684eb0e2ea1232a3
laramycarvalho <laramycarvalho@gmail.com>
2020-11-20 17:06:38 -0300

    20/11/2020

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:meus_carros_sinapse/app/app.dart';
 
 import 'package:meus_carros_sinapse/main.dart';
 
